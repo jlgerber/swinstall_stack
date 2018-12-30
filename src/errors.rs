ec8dd7cf2774573a8efe6cb4b18071ed53b03244
jonathan gerber <jlgerber@gmail.com>
2018-12-29 16:12:35 -1000

    add ParseIntError update Utf8Error to take String

--- src/errors.rs
+++ src/errors.rs
@@ -3,6 +3,7 @@ use failure::Fail;
 use std::convert::From;
 use std::str::Utf8Error;
 use chrono::format::ParseError;
+use std::num::ParseIntError;
 
 #[derive(Debug, Fail)]
 pub enum SwInstallError {
@@ -22,8 +23,8 @@ pub enum SwInstallError {
     ConvertOsStrFail,
     #[fail(display = "Missing attribute on elt tag")]
     MissingEltAttribute,
-    #[fail(display = "failed to convert to utf8")]
-    Utf8Error,
+    #[fail(display = "failed to convert to utf8: {}", _0)]
+    Utf8Error(String),
     #[fail(display = "chrono parse error: {}", _0)]
     ChronoParseError(String),
     #[fail(display = "runtime error: {}", _0)]
@@ -32,6 +33,8 @@ pub enum SwInstallError {
     InvalidDate(String),
     #[fail(display = "Invalid Time: {}", _0)]
     InvalidTime(String),
+    #[fail(display = "ParseIntError - failed to parse int: {}", _0)]
+    ParseIntError(String),
 }
 
 impl From<quick_xml::Error> for SwInstallError {
@@ -42,12 +45,18 @@ impl From<quick_xml::Error> for SwInstallError {
 
 impl From<Utf8Error> for SwInstallError {
     fn from(error: Utf8Error) -> Self {
-        SwInstallError::Utf8Error
+        SwInstallError::Utf8Error(error.to_string())
     }
 }
 
 impl From<ParseError> for SwInstallError {
     fn from(error: ParseError) -> Self {
         SwInstallError::ChronoParseError(error.cause().unwrap().to_string())
+    }
+}
+
+impl From<std::num::ParseIntError> for SwInstallError {
+    fn from(error: ParseIntError) -> Self {
+        SwInstallError::ParseIntError(error.to_string())
     }
 }
