73c9f7cbadf916a37edfad4357ea511cbea3d8e3
jonathan gerber <jlgerber@gmail.com>
2019-01-01 19:22:09 -1000

    change one::Elt.is_current to Pybool implement one::update

--- src/schemas/one.rs
+++ src/schemas/one.rs
@@ -55,18 +55,21 @@
 //!
 
 use chrono::{ NaiveDateTime };
-use crate::constants::DATETIME_FMT;
+use crate::constants::{ DATETIME_FMT, TAG_ELT };
 use crate::errors::SwInstallError;
 use crate::traits::{ SwinstallCurrent, SwinstallElement  };
 use crate::schemas;
 use crate::actions::Action;
+use crate::pybool::Pybool;
+
 #[allow(unused_imports)]
 use log::{debug, info, warn};
 use quick_xml::{
-    events::{attributes::Attributes, Event, },
+    events::{attributes::{ Attributes, Attribute }, Event, BytesStart },
     Reader,
     Writer,
 };
+
 use std::{
     cmp::PartialEq,
     str::{ FromStr, from_utf8 },
@@ -75,13 +78,13 @@ use std::{
 /// Model the elt tag contents from swinstall_log
 #[derive(Debug, PartialEq, Eq)]
 pub struct Elt {
-    pub is_current: bool,
+    pub is_current: Pybool,
     pub version: String,
     pub revision: Option<String>,
 }
 
 impl Elt {
-    pub fn new(is_current: bool, version: String) -> Self {
+    pub fn new(is_current: Pybool, version: String) -> Self {
         let mut pieces: Vec<String> = version.split("_").map(|x| x.to_string()).collect();
         let revision = if pieces.len() == 2 { pieces.pop() } else { None };
         let version = pieces.pop().unwrap_or("10000101-010101".to_string());
@@ -112,7 +115,7 @@ impl SwinstallElement  for Elt {
         //    convert to a str (from_utf8)
         //    convert to a String (to_string)
         let elt = Elt::new(
-            bool::from_str(
+            Pybool::from_str(
                 from_utf8(
                     &is_current
                     .ok_or(SwInstallError::MissingEltAttribute)?
@@ -142,61 +145,29 @@ impl SwinstallElement  for Elt {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn elt_from_attrs() {
-       let str_from = r#"<elt is_current="True" version="20161213-093146"/>"#;
-       let mut reader = Reader::from_str(str_from);
-       let mut buf = Vec::new();
-       loop {
-            match reader.read_event(&mut buf) {
-                        Ok(Event::Empty(ref e)) => {
-                            let elt = Elt::from_attrs( e.attributes()).expect("could not create elt");
-                            let expected = Elt {
-                                is_current: true,
-                                version: "20161213-093146".to_string(),
-                                revision: None
-                            };
-                            assert_eq!(elt, expected);
-                            break;
-                        }
-                        _ => {}
-            }
-        }
-    }
-
-    #[test]
-    fn elt_from_attrs_with_revision() {
-       let str_from = r#"<elt is_current="True" version="20161213-093146_r575055"/>"#;
-       let mut reader = Reader::from_str(str_from);
-       let mut buf = Vec::new();
-       loop {
-            match reader.read_event(&mut buf) {
-                        Ok(Event::Empty(ref e)) => {
-                            let elt = Elt::from_attrs( e.attributes()).expect("could not create elt");
-                            let expected = Elt {
-                                is_current: true,
-                                version: "20161213-093146".to_string(),
-                                revision: Some("r575055".to_string())
-                            };
-                            assert_eq!(elt, expected);
-                            break;
-                        }
-                        _ => {}
-            }
-        }
-    }
-}
-
 #[derive(Debug, Eq)]
 pub struct One;
 
 impl One {
     pub fn new() -> Self {
         One {}
+    }
+
+    // construct new ByteStart from supplied ELt
+    fn new_elem(&self, elem: &schemas::ReturnElt) -> BytesStart {
+        // extract Elt
+        let elem = match elem {
+            schemas::ReturnElt::One(e) => e,
+            _ => panic!("wrong type of ReturnELt"),
+        };
+
+        // define elem
+        let tag_vec = TAG_ELT.to_vec();
+        let tag_len = tag_vec.len();
+        let mut bselem = BytesStart::owned(tag_vec, tag_len);
+        bselem.push_attribute(Attribute::from(("is_current", elem.is_current.to_string().as_str())));
+        bselem.push_attribute(Attribute::from( ("version", elem.version.as_str()) ));
+        bselem
     }
 }
 
@@ -242,7 +213,7 @@ impl SwinstallCurrent for One {
                         let dt = NaiveDateTime::parse_from_str(version_str, DATETIME_FMT)?;
                         // update loop state variables
                         in_datetime =  dt <= *datetime;
-                        current = elt.is_current ;
+                        current = elt.is_current.as_bool() ;
                         debug!("current_at - Event::Empty - state vars: <in_datetime: {} current: {}>", in_datetime, current);
                         // we only update the last_elt if we are in the valid datetime range
                         // as specified by the user.
@@ -294,6 +265,131 @@ impl SwinstallCurrent for One {
         R: std::io::BufRead,
         W: std::io::Write
     {
-        Ok(())
+        // only need to generate this vars once
+        let tag_vec = TAG_ELT.to_vec();
+        let tag_len = tag_vec.len();
+
+        match action {
+            Action::Install => {
+                let mut buf = Vec::new();
+                let bselem = self.new_elem(&elem);
+
+                loop {
+                     match reader.read_event(&mut buf) {
+                        Ok(Event::Start(ref e)) => {
+                            writer.write_event(Event::Start(e.to_owned())).is_ok();
+                        }
+                        Ok(Event::Empty(ref e))  => {
+                            if e.name() == TAG_ELT {
+                                let elt = Elt::from_attrs( e.attributes())?;
+                                if elt.is_current.as_bool() == true {
+                                    // we need to set the ele to false
+                                    let mut new_elem = BytesStart::owned(tag_vec.clone(), tag_len);
+                                    new_elem.push_attribute(Attribute::from(("is_current", "False")));
+                                    new_elem.push_attribute(Attribute::from( ("version", elt.version.as_str()) ));
+                                    writer.write_event(Event::Empty(new_elem)).is_ok();
+
+                                } else {
+                                    writer.write_event(Event::Empty(e.to_owned())).is_ok();
+                                }
+                            } else {
+                                writer.write_event(Event::Empty(e.to_owned())).is_ok();
+                            }
+                        },
+                        Ok(Event::End(ref e))  => {
+                            writer.write_event(Event::End(e.to_owned())).is_ok();
+                        },
+                        Ok(Event::Eof) => {
+                            // we need to write out the current elt last...
+                            writer.write_event(Event::Empty(bselem)).is_ok();
+                            break
+                        },
+                        Ok(e) => assert!(writer.write_event(e).is_ok()),
+                        // or using the buffer
+                        // Ok(e) => assert!(writer.write(&buf).is_ok()),
+                        Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+                    }
+                    buf.clear();
+                }
+                Ok(())
+            }
+            _ => unimplemented!()
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{
+        io::Cursor,
+    };
+
+    #[test]
+    fn elt_from_attrs() {
+       let str_from = r#"<elt is_current="True" version="20161213-093146"/>"#;
+       let mut reader = Reader::from_str(str_from);
+       let mut buf = Vec::new();
+       loop {
+            match reader.read_event(&mut buf) {
+                        Ok(Event::Empty(ref e)) => {
+                            let elt = Elt::from_attrs( e.attributes()).expect("could not create elt");
+                            let expected = Elt {
+                                is_current: Pybool::new(true),
+                                version: "20161213-093146".to_string(),
+                                revision: None
+                            };
+                            assert_eq!(elt, expected);
+                            break;
+                        }
+                        _ => {}
+            }
+        }
+    }
+
+    #[test]
+    fn elt_from_attrs_with_revision() {
+       let str_from = r#"<elt is_current="True" version="20161213-093146_r575055"/>"#;
+       let mut reader = Reader::from_str(str_from);
+       let mut buf = Vec::new();
+       loop {
+            match reader.read_event(&mut buf) {
+                        Ok(Event::Empty(ref e)) => {
+                            let elt = Elt::from_attrs( e.attributes()).expect("could not create elt");
+                            let expected = Elt {
+                                is_current: Pybool::new(true),
+                                version: "20161213-093146".to_string(),
+                                revision: Some("r575055".to_string())
+                            };
+                            assert_eq!(elt, expected);
+                            break;
+                        }
+                        _ => {}
+            }
+        }
+    }
+
+
+    #[test]
+    fn update_one() {
+        let two = One::new();
+        let swinstall_stack_elt_tags = r#"<elt is_current="True" version="20180702-144204"/>"#;
+
+        let mut writer = Writer::new(Cursor::new(Vec::new()));
+        let mut reader = Reader::from_str(swinstall_stack_elt_tags);
+        let action = Action::Install;
+        let elem = Elt::new(Pybool::True, "20190101-113000".to_string());
+
+        let result = two.update( action,
+            &mut reader,
+            &mut writer,
+            schemas::ReturnElt::One(elem)
+        );
+        assert_eq!(result.unwrap(), ());
+        let result = writer.into_inner().into_inner();
+        let result = String::from_utf8(result).unwrap();
+        let expected = r#"<elt is_current="False" version="20180702-144204"/><elt is_current="True" version="20190101-113000"/>"#;
+        assert_eq!(result.as_str(), expected);
     }
 }
